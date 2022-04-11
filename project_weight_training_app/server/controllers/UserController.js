0cfc0e805cc49b0ab6ec8ee80abea80fd1deb521
colbel2 <colbel22@gmail.com>
2022-04-11 11:57:12 -0500

    Checkpoint: Login Reg, async, await and userToken implemented in controller file

--- project_weight_training_app/server/controllers/UserController.js
+++ project_weight_training_app/server/controllers/UserController.js
@@ -36,78 +36,70 @@ class UserController {
 
 
 
+    // register = (req, res) => {
+    //     User.create(req.body)
+    //         .then(user => {
+    //             res.json({ msg: "success!", user: user });
+    //         })
+    //         .catch(err => res.json(err));
+    // }
+
+    //REPLACED register above with register below
+
     register = (req, res) => {
         User.create(req.body)
             .then(user => {
-                res.json({ msg: "success!", user: user });
+                const userToken = jwt.sign({
+                    id: user._id
+                }, process.env.SECRET_KEY);
+
+                res
+                    .cookie("usertoken", userToken, process.env.SECRET_KEY, {
+                        httpOnly: true
+                    })
+                    .json({ msg: "success!", user: user });
             })
             .catch(err => res.json(err));
     }
+    login = async (req, res) => {
+        const user = await User.findOne({ email: req.body.email });
+
+        if (user === null) {
+            // email not found in users collection
+            return res.sendStatus(400);
+        }
+
+        // if we made it this far, we found a user with this email address
+        // let's compare the supplied password to the hashed password in the database
+        const correctPassword = await bcrypt.compare(req.body.password, user.password);
+
+        if (!correctPassword) {
+            // password wasn't a match!
+            return res.sendStatus(400);
+        }
+
+        // if we made it this far, the password was correct
+        const userToken = jwt.sign({
+            id: user._id
+        }, process.env.SECRET_KEY);
+
+        // note that the response object allows chained calls to cookie and json
+        res
+            .cookie("usertoken", userToken, process.env.SECRET_KEY, {
+                httpOnly: true
+            })
+            .json({ msg: "success!" });
+    }
+
+    logout = (req, res) => {
+        res.clearCookie('usertoken');
+        res.sendStatus(200);
+    }
+
+
+
+
+
 }
 
-
 module.exports = new UserController();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// module.exports.findOne = (req, res) => {
-//     Ninja.findOne({_id: req.params.id})
-//         .then(oneNinja => {
-//             res.json({results: oneNinja})
-//         })
-//         .catch(err => res.json({message: "Something went wrong", error: err}))
-// }
-
-// module.exports.createNewNinja = (req,res) => {
-//     console.log("trying to create new ninja")
-//     console.log("REQ.BODY---->", req.body)
-//     Ninja.create(req.body)
-//         .then(newlyCreatedNinja =>
-//             res.json({results: newlyCreatedNinja}))
-//         .catch(err => res.json({message: "Something went wrong", error: err}))
-// }
-
-// module.exports.updateNinja = (req,res) => {
-//     Ninja.findByIdAndUpdate(
-//         {_id: req.params.id},
-//         req.body,
-//         {new: true, runValidators: true})
-//         .then(updatedNinja => {
-//             res.json({results: updatedNinja})
-//         })
-//         .catch(err => res.json({message: "Something went wrong", error: err}))
-// }
-
-// module.exports.deleteNinja = (req,res) => {
-//     Ninja.findByIdAndDelete({_id: req.params.id})
-//         .then(deletedNinja => {
-//             res.json({results:deletedNinja})
-//         })
-//         .catch(err => res.json({message: "Something went wrong", error: err}))
-// }
-
-// module.exports.findRandomNinja = (req,res) =>{
-//     console.log("trying to find random ninja")
-//     function getRandomInt(max){
-//         return Math.floor(Math.random() * max);
-//     }
-//     Ninja.find()
-//         .then(allNinjas=>{
-//             let randomIndex= getRandomInt(allNinjas.length)
-//             res.json({results:allNinjas[randomIndex]})
-//         })
-//         .catch(err => res.json({message: "Something went wrong", error: err}))
-// }
