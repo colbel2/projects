2915c4e638951f6d3c7773b8b2e915bfe96be22a
colbel2 <colbel22@gmail.com>
2022-04-11 11:37:43 -0500

    Checkpoint: Login and Reg, introduced cookie-parcer and jsonwebtoken to app

--- project_weight_training_app/server.js
+++ project_weight_training_app/server.js
@@ -4,14 +4,18 @@ const cors = require("cors")
 require('dotenv').config(); //used for reading my secret key in .env and protected by .gitignore
 
 console.log("Secret key is ----->", process.env.SECRET_KEY) //test to show key is read by server.js
+const cookieParser = require('cookie-parser'); //so the server can understand cookie information coming in from the client (browser)
+
 
 const app = express(); //initialize express
 const port = 8000; // specify port in a variable
 
+
+
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-app.use(cors())
-
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}))
+app.use(cookieParser());
 //require makes it where all the code from the file u are requiring goes on this line
 require("./server/config/config")
 
