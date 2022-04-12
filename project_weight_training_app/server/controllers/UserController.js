b2612808d8be132a66a7872a955d33c58758ad4a
colbel2 <colbel22@gmail.com>
2022-04-11 21:54:32 -0500

    Checkpoint: Login and Reg, duplicate email prevention working

--- project_weight_training_app/server/controllers/UserController.js
+++ project_weight_training_app/server/controllers/UserController.js
@@ -37,21 +37,72 @@ class UserController {
 
     //REPLACED register above with register below
 
-    register = (req, res) => {
-        User.create(req.body)
-            .then(user => {
-                const userToken = jwt.sign({
-                    id: user._id
-                }, process.env.SECRET_KEY);
+    // register = (req, res) => {
+    //     User.find({ email: req.body.email })
+    //         .then(usersWithExistingEmail => {
+    //             console.log("response when finding a user with an already existing email", usersWithExistingEmail)
+    //             if (usersWithExistingEmail.length === 0) {
 
-                res
-                    .cookie("usertoken", userToken, process.env.SECRET_KEY, {
-                        httpOnly: true
-                    })
-                    .json({ msg: "success!", user: user });
+    //             }
+    //         })
+    //         .catch(err => console.log("ERROR:", err))
+
+
+    //     User.create(req.body)
+    //         .then(user => {
+    //             const userToken = jwt.sign({
+    //                 id: user._id
+    //             }, process.env.SECRET_KEY);
+
+    //             res
+    //                 .cookie("usertoken", userToken, process.env.SECRET_KEY, {
+    //                     httpOnly: true
+    //                 })
+    //                 .json({ msg: "success!", user: user });
+    //         })
+    //         .catch(err => res.json(err));
+    // }
+
+    register = (req, res) => {
+        User.find({ email: req.body.email })
+            .then(usersWithExistingEmail => {
+                console.log("response when finding a user with an already existing email", usersWithExistingEmail)
+                if (usersWithExistingEmail.length === 0) { //this means the email is not yet taken and we can create a user with this email
+                    User.create(req.body)
+                        .then(user => {
+                            //when the .then() happens that means taht the user from the form was successsfully created and is stored in that variable "user" which has info about the user that was just put into the db, including the field _id
+                            const userToken = jwt.sign({
+                                id: user._id,
+                                firstName: user.firstName
+                            }, process.env.SECRET_KEY);
+
+                            //respond with a cookie called "usertoken" which contains the JWT from above called userTokenJWT AND also respond with json with info abou the user who just got created
+                            res
+                                .cookie("usertoken", userToken, process.env.SECRET_KEY, {
+                                    httpOnly: true
+                                })
+                                .json({ msg: "success!", user: user });
+                        })
+                        .catch(err => res.json(err));
+                } else {
+                    //else --> the email is already taken so we will send back an error message
+                    res.json({ errors: { email: { message: "This Email Address Has Already Been Registered. Try Logging In!" } } })
+                }
             })
-            .catch(err => res.json(err));
+            .catch(err => console.log("errr!", err))
+
+
     }
+
+
+
+
+
+
+
+
+
+
     login = async (req, res) => {
         const user = await User.findOne({ email: req.body.email });
 
